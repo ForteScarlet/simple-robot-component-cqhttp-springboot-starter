646eb02759bdc1a4d654b9786385501b78753697
ForteScarlet <ForteScarlet@163.com>
2020-06-18 16:14:53 +0800

    update to 1.13.0 core starter

--- src/main/java/com/simplerobot/component/springboot/configuration/CQHttpSpringConfiguration.java
+++ src/main/java/com/simplerobot/component/springboot/configuration/CQHttpSpringConfiguration.java
@@ -1,8 +1,12 @@
 package com.simplerobot.component.springboot.configuration;
 
 import com.forte.component.forcoolqhttpapi.CoolQHttpApplication;
+import com.forte.qqrobot.Application;
+import com.forte.qqrobot.BaseApplication;
+import com.forte.qqrobot.anno.depend.Beans;
 import com.simplerobot.core.springboot.configuration.SpringBootDependGetter;
 import com.simplerobot.core.springboot.configuration.SpringbootQQLogBack;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +17,13 @@ import org.springframework.context.annotation.Configuration;
 public class CQHttpSpringConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean(Application.class)
     public CQHttpSpringBootBeanFactoryApp getCQHttpApp(SpringBootDependGetter dependGetter) {
         return new CQHttpSpringBootBeanFactoryApp(dependGetter);
     }
 
     @Bean
+    @ConditionalOnMissingBean(BaseApplication.class)
     public CoolQHttpApplication getCQHttpApplication(SpringbootQQLogBack logBack) {
         return new CoolQHttpApplication(logBack.getLogBack());
     }
